55a729dd8a00a6b36594b69f1f3f690481a48078
Abhijith Prasanth <abhijith243121@gmail.com>
2026-08-21 15:39:29 +0530

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_map<char, int>> rows(9);
+        vector<unordered_map<char, int>> column(9);
+        vector<unordered_map<char, int>> boxes(9);
+        bool flag = true;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                int bnum = (i/3)*3 + (j/3);
+                if ((rows[i][board[i][j]] + column[j][board[i][j]] + boxes[bnum][board[i][j]]) > 0) {
+                    flag = false;
+                    break;
+                }
+                rows[i][board[i][j]]++;
+                column[j][board[i][j]]++;
+                boxes[bnum][board[i][j]]++;
+            }
+        }
+        return flag;
+    }
+};
